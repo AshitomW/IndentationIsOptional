bd357b21796a3be0ef70a337cfa03346578126fd
Ashitom <sasukeuchia32828@gmail.com>
2026-03-21 20:19:07 +0545

    checker header

---
+++ include/iio/checker.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+
+#include <iio/utils.hpp>
+#include <string>
+#include <string_view>
+#include <istream>
+#include <optional>
+#include <vector>
+
+
+
+namespace iio{
+  class Checker{
+
+    private:
+    int depth_ = 0; 
+    int line_num_ = 0;
+
+
+    public:
+      Checker() = default;
+      Checker(const Checker&) = delete;
+      Checker& operator=(const Checker&) = delete; 
+      Checker& operator=(Checker&&) = default;
+
+    
+      [[nodiscard]] std::optional<Diagnostic> feed_line(std::string_view line);
+      [[nodiscard]] std::optional<Diagnostic> finalize() const;
+      [[nodiscard]] int depth() const noexcept {return depth_;}
+      [[nodiscard]] int line_number() const noexcept {return line_num_;}
+      void reset() noexcept;
+
+  };
+
+
+  [[nodiscard]] std::optional<Diagnostic> check_stream(std::istream& input);
+  [[nodiscard]] std::optional<Diagnostic> check_file(const std::string& filtepath);
+
+}
