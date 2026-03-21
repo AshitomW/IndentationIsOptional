09aee5673f868477abac1c1d62c079bbbad5411c
Ashitom <sasukeuchia32828@gmail.com>
2026-03-21 20:13:02 +0545

    utils

--- include/iio/utils.hpp
+++ include/iio/utils.hpp
@@ -1,6 +1,5 @@
 
 #pragma once
-
 #include <string>
 #include <string_view>
 #include <cstdint>
@@ -26,4 +25,24 @@ namespace iio{
     return std::holds_alternative<T>(r);
   }
 
+  template <typename T>
+  [[nodiscard]] inline const T& get_ok(const Result<T>& r){
+    return std::get<T>(r);
+  }
+
+
+  template <typename T>
+  [[nodiscard]] inline const Diagnostic& get_err(const Result<T>& r){
+    return std::get<Diagnostic>(r);
+  }
+
+  [[nodiscard]] std::string make_indent(int level);
+  [[nodiscard]] std::string_view trim_left(std::string_view sv);
+  [[nodiscard]] std::string_view trim_right(std::string_view sv);
+  [[nodiscard]] std::string_view trim(std::string_view sv);
+  [[nodiscard]] bool is_blank(std::string_view sv);
+  [[nodiscard]] std::size_t leading_whitespace_count(std::string_view sv);
+  [[nodiscard]] bool is_inside_string(std::string_view line, std::size_t pos);
+  [[nodiscard]] std::size_t find_brace_outside_string(std::string_view line, char brace);
+  [[nodiscard]] int count_braces_outside_string(std::string_view line,char brace);
 }
